c4decf93f36f1bc8c88648cf4f2eec4bdd52b67b
SidClark576 <sidneyordonia@gmail.com>
2025-05-30 23:31:35 -1000

    edited files

--- Activity-04/build-vbox.ps1
+++ Activity-04/build-vbox.ps1
@@ -1,22 +1,12 @@
-# Create and register a new VM
-& "C:\Program Files\Oracle\VirtualBox\VBoxManage.exe" createvm --name "AutomatedWin10" --register
+#### **For VirtualBox:** (`build-vbox.ps1`)
 
-# Set memory, CPU, and OS type
-& "C:\Program Files\Oracle\VirtualBox\VBoxManage.exe" modifyvm "AutomatedWin10" --memory 4096 --cpus 2 --ostype "Windows10_64"
 
-# Create virtual hard disk
-& "C:\Program Files\Oracle\VirtualBox\VBoxManage.exe" createmedium disk --filename "C:\ISO Folder\AutomatedWin10.vdi" --size 40000
-
-# Add SATA controller and attach virtual hard disk
-& "C:\Program Files\Oracle\VirtualBox\VBoxManage.exe" storagectl "AutomatedWin10" --name "SATA Controller" --add sata --controller IntelAhci
-& "C:\Program Files\Oracle\VirtualBox\VBoxManage.exe" storageattach "AutomatedWin10" --storagectl "SATA Controller" --port 0 --device 0 --type hdd --medium "C:\ISO Folder\AutomatedWin10.vdi"
-
-# Add IDE controller and attach Windows ISO
-& "C:\Program Files\Oracle\VirtualBox\VBoxManage.exe" storagectl "AutomatedWin10" --name "IDE Controller" --add ide
-& "C:\Program Files\Oracle\VirtualBox\VBoxManage.exe" storageattach "AutomatedWin10" --storagectl "IDE Controller" --port 0 --device 0 --type dvddrive --medium "C:\ISO Folder\en-us_windows_10_consumer_editions_version_22h2_x64_dvd_8da72ab3.iso"
-
-# Attach answer.iso to second IDE port
-& "C:\Program Files\Oracle\VirtualBox\VBoxManage.exe" storageattach "AutomatedWin10" --storagectl "IDE Controller" --port 1 --device 0 --type dvddrive --medium "C:\ISO Folder\answer.iso"
-
-# Start the virtual machine
-& "C:\Program Files\Oracle\VirtualBox\VBoxManage.exe" startvm "AutomatedWin10"
+& "D:\VirualBox\VBoxManage.exe" createvm --name "AutomatedWin10" --register
+& "D:\VirualBox\VBoxManage.exe" modifyvm "AutomatedWin10" --memory 4096 --cpus 2 --ostype "Windows10_64"
+& "D:\VirualBox\VBoxManage.exe" createmedium disk --filename "C:\ISO Folder\AutomatedWin10.vdi" --size 40000
+& "D:\VirualBox\VBoxManage.exe" storagectl "AutomatedWin10" --name "SATA Controller" --add sata --controller IntelAhci
+& "D:\VirualBox\VBoxManage.exe" storageattach "AutomatedWin10" --storagectl "SATA Controller" --port 0 --device 0 --type hdd --medium "C:\ISO Folder\AutomatedWin10.vdi"
+& "D:\VirualBox\VBoxManage.exe" storagectl "AutomatedWin10" --name "IDE Controller" --add ide
+& "D:\VirualBox\VBoxManage.exe" storageattach "AutomatedWin10" --storagectl "IDE Controller" --port 0 --device 0 --type dvddrive --medium "C:\ISO Folder\Windows+11+Dev+(OS+Build+21996.1).iso"
+& "D:\VirualBox\VBoxManage.exe" storageattach "AutomatedWin10" --storagectl "IDE Controller" --port 1 --device 0 --type dvddrive --medium "C:\ISO Folder\answer.iso"
+& "D:\VirualBox\VBoxManage.exe" startvm "AutomatedWin10"
