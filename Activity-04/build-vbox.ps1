bd5161ef02171f07288dda5dcdfa256c4f2b5f73
SidClark576 <sidneyordonia@gmail.com>
2025-06-02 10:40:50 -1000

    edited vbox script

--- Activity-04/build-vbox.ps1
+++ Activity-04/build-vbox.ps1
@@ -1,12 +1,13 @@
 #### **For VirtualBox:** (`build-vbox.ps1`)
 
 
-& "D:\VirualBox\VBoxManage.exe" createvm --name "AutomatedWin10" --register
-& "D:\VirualBox\VBoxManage.exe" modifyvm "AutomatedWin10" --memory 4096 --cpus 2 --ostype "Windows10_64"
-& "D:\VirualBox\VBoxManage.exe" createmedium disk --filename "C:\ISO Folder\AutomatedWin10.vdi" --size 40000
-& "D:\VirualBox\VBoxManage.exe" storagectl "AutomatedWin10" --name "SATA Controller" --add sata --controller IntelAhci
-& "D:\VirualBox\VBoxManage.exe" storageattach "AutomatedWin10" --storagectl "SATA Controller" --port 0 --device 0 --type hdd --medium "C:\ISO Folder\AutomatedWin10.vdi"
-& "D:\VirualBox\VBoxManage.exe" storagectl "AutomatedWin10" --name "IDE Controller" --add ide
-& "D:\VirualBox\VBoxManage.exe" storageattach "AutomatedWin10" --storagectl "IDE Controller" --port 0 --device 0 --type dvddrive --medium "C:\ISO Folder\Windows+11+Dev+(OS+Build+21996.1).iso"
-& "D:\VirualBox\VBoxManage.exe" storageattach "AutomatedWin10" --storagectl "IDE Controller" --port 1 --device 0 --type dvddrive --medium "C:\ISO Folder\answer.iso"
-& "D:\VirualBox\VBoxManage.exe" startvm "AutomatedWin10"
+& "C:\Program Files\Oracle\VirtualBox\VBoxManage.exe" createvm --name "AutomatedWin10" --register
+& "C:\Program Files\Oracle\VirtualBox\VBoxManage.exe" modifyvm "AutomatedWin10" --memory 4096 --cpus 2 --ostype "Windows10_64"
+& "C:\Program Files\Oracle\VirtualBox\VBoxManage.exe" createmedium disk --filename "C:\ISO Folder\AutomatedWin10.vdi" --size 40000
+& "C:\Program Files\Oracle\VirtualBox\VBoxManage.exe" storagectl "AutomatedWin10" --name "SATA Controller" --add sata --controller IntelAhci
+& "C:\Program Files\Oracle\VirtualBox\VBoxManage.exe" storageattach "AutomatedWin10" --storagectl "SATA Controller" --port 0 --device 0 --type hdd --medium "C:\ISO Folder\AutomatedWin10.vdi"
+& "C:\Program Files\Oracle\VirtualBox\VBoxManage.exe" storagectl "AutomatedWin10" --name "IDE Controller" --add ide
+& "C:\Program Files\Oracle\VirtualBox\VBoxManage.exe" storageattach "AutomatedWin10" --storagectl "IDE Controller" --port 0 --device 0 --type dvddrive --medium "C:\ISO Folder\en-us_windows_10_consumer_editions_version_22h2_x64_dvd_8da72ab3.iso"
+& "C:\Program Files\Oracle\VirtualBox\VBoxManage.exe" storageattach "AutomatedWin10" --storagectl "IDE Controller" --port 1 --device 0 --type dvddrive --medium "C:\ISO Folder\answer.iso"
+& "C:\Program Files\Oracle\VirtualBox\VBoxManage.exe" modifyvm "AutomatedWin10" --nic1 nat
+& "C:\Program Files\Oracle\VirtualBox\VBoxManage.exe" startvm "AutomatedWin10"
